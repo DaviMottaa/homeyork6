aafee706b6826fe47ad46a2467dbd5886022fa13
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-06-30 08:51:07 -0300

    Add files via upload

---
+++ Davi_300621.py
@@ -0,0 +1,66 @@
+#Question1
+Lista = ["Arroz", "Feijão", "Macarrão", "Pão", "Estrogronofe"]
+
+#Question2
+print(Lista)
+
+#Question3
+print("3***********************")
+for item in Lista:
+    print(item)
+
+#Question4
+print("4*********************")
+for item in Lista:
+    print(item+" "+item)
+
+#Question5
+print("5*******************************")
+for item in Lista:
+    print("I am going to eat")
+    print(item)
+
+#Question6
+print("6***********************************")
+for item in Lista:
+    print(item)
+    print("That is what I just ate")
+
+#Qestion7
+print("7**********************************")
+print("list of things I am going to eat")
+for item in Lista:
+    print(item)
+
+#Question8
+print("8**************************************")
+for item in Lista:
+    print(item)
+print("This was the list of things I ate")
+
+#Question9
+print("9****************************")
+numbers = list(range(1,16))
+
+#Question10
+print(numbers)
+
+#Question11
+print("11***********************************")
+for item in range(6):
+    print("Davi")
+
+#Question12
+print("12***********************************")
+for item in range(3,22,3):
+    print(item)
+
+#Question13
+print("13***********************************")
+for item in range(10,3,-1):
+    print(item)
+
+#Question14
+print("14***********************************")
+for item in range(11,-4,-2):
+    print(item)
